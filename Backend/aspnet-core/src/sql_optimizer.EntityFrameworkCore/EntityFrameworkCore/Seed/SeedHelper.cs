601ce9f8de9b1c1385cf2a3c7881b6900d1ef89e
Teboho02 <86244195+Teboho02@users.noreply.github.com>
2026-03-26 13:52:09 +0200

    fix: update db

--- Backend/aspnet-core/src/sql_optimizer.EntityFrameworkCore/EntityFrameworkCore/Seed/SeedHelper.cs
+++ Backend/aspnet-core/src/sql_optimizer.EntityFrameworkCore/EntityFrameworkCore/Seed/SeedHelper.cs
@@ -21,9 +21,6 @@ public static class SeedHelper
     {
         context.SuppressAutoSetTenantId = true;
 
-        // apply any pending EF Core migrations before seeding
-        context.Database.Migrate();
-
         // Host seed
         new InitialHostDbBuilder(context).Create();
 
--- Backend/aspnet-core/src/sql_optimizer.EntityFrameworkCore/EntityFrameworkCore/sql_optimizerDbContextConfigurer.cs
+++ Backend/aspnet-core/src/sql_optimizer.EntityFrameworkCore/EntityFrameworkCore/sql_optimizerDbContextConfigurer.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using System.Data.Common;
 
 namespace sql_optimizer.EntityFrameworkCore;
@@ -8,10 +9,12 @@ public static class sql_optimizerDbContextConfigurer
     public static void Configure(DbContextOptionsBuilder<sql_optimizerDbContext> builder, string connectionString)
     {
         builder.UseNpgsql(connectionString);
+        builder.ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning));
     }
 
     public static void Configure(DbContextOptionsBuilder<sql_optimizerDbContext> builder, DbConnection connection)
     {
         builder.UseNpgsql(connection);
+        builder.ConfigureWarnings(w => w.Ignore(RelationalEventId.PendingModelChangesWarning));
     }
 }
